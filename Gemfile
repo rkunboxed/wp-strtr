2c2025d19c44499330d9d7c732a70dc64f69b033
Andrew Kamm <ak@kamm.co>
2015-07-30 10:14:02 -0500

    Initial customizations, still unstyled

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source 'https://rubygems.org'
+
+gem 'compass', '~> 1.0.3'
+gem 'compass-import-once', '~> 1.0.5'
+gem 'susy'
+gem 'compass-pug', '~> 0.1'
---
+++ config.rb
@@ -0,0 +1,12 @@
+# Configuration file for Compass.
+
+css_dir = 'assets/style.compiled/'
+sass_dir = 'assets/style.source/'
+fonts_dir = 'assets/fonts/'
+images_dir = 'assets/img/'
+relative_assets = true
+output_style = :compressed
+
+require 'compass/import-once'
+require 'susy'
+require 'compass-pug'
