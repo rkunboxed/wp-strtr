aa922e3c5e9cfa964b7556e342c13a583303b876
Andrew Kamm <ak@kamm.co>
2015-08-03 08:35:56 -0500

    Various layout, size, text updates

--- header.php
+++ header.php
@@ -25,11 +25,7 @@
 	<header id="masthead" class="site-header" role="banner">
 		<div class="inner for-masthead">
 			<div class="site-branding">
-				<?php if ( is_front_page() && is_home() ) : ?>
-					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php else : ?>
-					<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php endif; ?>
+				<div class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></div>
 				<p class="site-description"><?php bloginfo( 'description' ); ?></p>
 			</div><!-- .site-branding -->
 
